465b2f2b5273d6b4aa2c7a2ed389d739ad7c2325
irwinekabakti <irwinekabakti@gmail.com>
2024-11-11 11:49:51 +0800

    oop1

---
+++ src/Cars.java
@@ -0,0 +1,53 @@
+public class Cars {
+    private String make;
+    private String model;
+    private int year;
+    private double price;
+
+    public Cars(String make, String model, int year, double price) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void displayInfo() {
+        System.out.println("Brand: " + make + ", Model: " + model + ", Year: " + year + ", Price: $" + price);
+    }
+
+    public void startEngine() {
+        System.out.println("The car's engine starts with a basic sound.");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Cars car1 = new Cars("Toyota", "Camry", 2020, 24000);
+        car1.displayInfo();
+        car1.startEngine();
+
+        System.out.println();
+    }
+}
