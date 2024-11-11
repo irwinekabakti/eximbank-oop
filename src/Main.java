1706d9ba69236038380c3aef6bd167f911e54fff
irwinekabakti <irwinekabakti@gmail.com>
2024-11-11 15:52:18 +0800

    example-polymorphism

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println();
 
         ElectricVehicles electricCar1 = new ElectricVehicles("Tesla", "Model S", 2022, 79999, 100, 396);
-        electricCar1.displayInfo();
-        electricCar1.startEngine();
+        electricCar1.displayInfo();  // call the inheritance methods using polymorphism, but will overriding the methods from parents and have different task
+        electricCar1.startEngine();  // call the inheritance methods using polymorphism, but will overriding the methods from parents and have different task
     }
 }
